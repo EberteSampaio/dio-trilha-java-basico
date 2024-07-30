6860e3a6f79bd9963d3d2a83a7d19a0002776df6
EberteSampaio <ebertechavessampaiojr@gmail.com>
2024-07-30 18:37:58 -0300

    feat: implementando tratativas de erros do usuário.

--- ContaBanco/src/example/main/ContaTerminal.java
+++ ContaBanco/src/example/main/ContaTerminal.java
@@ -1,28 +1,83 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import example.model.Conta;
 
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
 
-        Scanner input = new Scanner(System.in);
+        try{
+            Scanner input = new Scanner(System.in);
 
-        System.out.println("Olá, para realizar o seu cadastro, informe seu nome completo!");
-        String nome = input.nextLine();
+            System.out.println("Olá, para realizar o seu cadastro, informe seu nome completo!");
+            String nome = input.nextLine();
+
+            validarNome(nome);
+                
+            System.out.println("Excelente! Agora preciso do número da sua agência. \nExemplo: 067-8");
+            String agencia = input.nextLine();
+
+            validarAgencia(agencia);
+
+            System.out.println("Falta só um pouco para finalizarmos... Agora preciso do seu número da conta. \nExemplo: 1021");
+            int numeroConta = input.nextInt();
+
+            validarNumeroConta(numeroConta);
+
+            System.out.println("Para finalizar, informe seu saldo!");
+            double saldoConta = input.nextDouble();
+
+            validarSaldo(saldoConta);
+
+            Conta novaConta = new Conta(numeroConta, agencia, nome, saldoConta);
+
+            System.out.println("Olá "+ novaConta.getNomeCliente() +", obrigado por criar uma conta em nosso banco, sua agência é "+ novaConta.getAgencia() +", conta "+ novaConta.getNumero() +" e seu saldo "+ novaConta.getSaldo() +" já está disponível para saque");
             
-        System.out.println("Excelente! Agora preciso do número da sua agência. \nExemplo: 067-8");
-        String agencia = input.nextLine();
+            input.close(); 
 
-        System.out.println("Falta só um pouco para finalizarmos... Agora preciso do seu número da conta.");
-        int numeroConta = input.nextInt();
+        } catch (InputMismatchException |IllegalArgumentException exception) {
 
-        System.out.println("Para finalizar, informe seu saldo!");
-        double saldoConta = input.nextDouble();
+            String msg = (exception.getMessage() == null)? "Aceitamos apenas no formato informado no exemplo.": exception.getMessage();
 
+            System.out.println(msg +" Tente novamente!");
+        }
+        
+    
+    }
 
-        Conta novaConta = new Conta(numeroConta, agencia, nome, saldoConta);
+     public static void validarAgencia(String agencia) throws IllegalArgumentException {
 
+        Pattern formatoAgencia = Pattern.compile("\\d{3}-\\d");
+        Matcher validaFormato = formatoAgencia.matcher(agencia);
 
-        System.out.println("Olá "+ novaConta.getNomeCliente() +", obrigado por criar uma conta em nosso banco, sua agência é "+ novaConta.getAgencia() +", conta "+ novaConta.getNumero() +" e seu saldo "+ novaConta.getSaldo() +" já está disponível para saque");
+        if (!validaFormato.matches()) {
+            throw new IllegalArgumentException("O formato da agência precisar seguir o exemplo.");
+        }
+    }
+
+    public static void validarNumeroConta(int numeroConta) {
+
+        Pattern formatoNumeroConta = Pattern.compile("\\d{4}");
+        Matcher validaFormato = formatoNumeroConta.matcher(String.valueOf(numeroConta));
+
+        if (!validaFormato.matches()) {
+            throw new IllegalArgumentException("O formato da agência precisar seguir o exemplo.");
+        }
+    }
+
+    public static void validarNome(String nome) {
+
+        if(nome.equals("") || nome == null){
+            throw new IllegalArgumentException("É necessário preencher o nome.");
+        }
+    }
+
+    public static void validarSaldo(double saldoConta) {
+
+        if(saldoConta < 0){
+            throw new IllegalArgumentException("Não é aceito valores menores que zero.");  
+        }
     }
 }
